8248113b1a0901675d9c5b826af10140d0a99a3e
sasha <56977617+aparf@users.noreply.github.com>
2021-12-16 15:38:16 +0300

    added front end and simplified the code logic

--- js/script.js
+++ js/script.js
@@ -1,52 +1,48 @@
 window.addEventListener("load",function(){
-
-	// parse data from given url
 	function parseData(url){
-		// money.innerHTML = Math.floor(Math.random() * 100000); //parseData
 		let xhr = new XMLHttpRequest();
-
 		xhr.open('GET', url, false);
 		xhr.send();
-
 		if (xhr.status == 200){
-			var totalSpendings = JSON.parse(xhr.responseText)['oops_total'];
+			var totalSpendings = Math.floor(JSON.parse(xhr.responseText)['oops_total']);
 			return(totalSpendings);
 		} else
 			console.log(xhr.status + ' ' + xhr.satusText);
 	}
 
-	// nicely increasing the money timer on website and increase currentValue
-	function increaseValue(increase){
+	function increaseValue(increase, currentValue){
 		let needTime = increase / 10;
+		increasedValue = currentValue + increase;
 		const countDown = setInterval(()=>{
-			money.innerHTML++;
-			console.log('current value ', currentValue, '; current difference ', money.innerHTML, ' current difference:', currentValue-money.innerHTML);
-			if ((currentValue) - parseInt(money.innerHTML) == 0){
+			currentValue++;
+			money.innerHTML = currentValue.toLocaleString("en-US", { 
+			    style: "currency", 
+			    currency: "USD",
+			    maximumFractionDigits: 0
+		  	});
+			if ((increasedValue) - currentValue == 0)
 				clearInterval(countDown);
-			}
-		}, 1000 * needTime / increase)
-		currentValue += increase;
+		}, 1000 * needTime / increase);
+		return (increasedValue);
 	}
 
-	// checking whether new Data added
-	function checkNew(url){
+	function checkNew(url, currentValue){
 		let newValue = parseData(url) + 100;
-		console.log('new value == ', newValue, ' currentValue == ', currentValue);
-		if (newValue != currentValue){
-			console.log("no!");
+		if (newValue != currentValue)
 			increaseValue(newValue - currentValue);
-		}
+		
 	}
 
-	// initalize the variables
 	const money = document.querySelector(".money");
 	let url = 'https://dev.oops.finance/api/v1/public/oops_total';
-	let currentValue = parseInt(parseData(url)); //parse initial value
-	money.innerHTML = currentValue;
-
+	let value = parseInt(parseData(url)); //parse initial value
+	money.innerHTML = value.toLocaleString("en-US", { 
+		style: "currency", 
+		currency: "USD",
+		maximumFractionDigits: 0
+	});
 	let difference = 100; // initial difference in money to make nice animation
 
-	increaseValue(difference);
-	console.log(currentValue);
-	setInterval(checkNew, 25000, url); // change the time when it parses new data
+	value = increaseValue(difference, value);
+	setInterval(checkNew, 60000, url, value); // change the time when it parses new data
 })
